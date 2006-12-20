04b95bab83992fe0b9975967b7069927b7cf391a
Sven Meier <sven@meiers.net>
2006-12-20 22:41:05 +0000

    showing platform information

--- jorgan/src/main/java/jorgan/gui/AboutPanel.java
+++ jorgan/src/main/java/jorgan/gui/AboutPanel.java
@@ -25,7 +25,6 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.util.ResourceBundle;
 
-import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
@@ -35,108 +34,144 @@ import javax.swing.JWindow;
 import javax.swing.SwingUtilities;
 
 import jorgan.App;
+import jorgan.util.SystemInfo;
 
 /**
  * Panel that displays information about jOrgan.
  */
 public class AboutPanel extends JPanel {
 
-    private static ResourceBundle resources = ResourceBundle
-            .getBundle("jorgan.gui.resources");
+	private static ResourceBundle resources = ResourceBundle
+			.getBundle("jorgan.gui.resources");
 
-    /**
-     * The icon used to display the about image.
-     */
-    private ImageIcon icon = new ImageIcon(getClass().getResource(
-            "img/about.gif"));
+	/**
+	 * The label used to display the version of jOrgan.
+	 */
+	private JLabel label = new JLabel();
 
-    /**
-     * The label used to display the version of jOrgan.
-     */
-    private JLabel label = new JLabel();
+	/**
+	 * Creata an about panel.
+	 */
+	public AboutPanel() {
+		setLayout(new BorderLayout());
 
-    /**
-     * Creata an about panel.
-     */
-    public AboutPanel() {
-        setLayout(new BorderLayout());
+		label.setIcon(createIcon());
+		add(label);
+	}
 
-        BufferedImage image = new BufferedImage(icon.getIconWidth(), icon
-                .getIconHeight(), BufferedImage.TYPE_INT_RGB);
-        Graphics2D g = (Graphics2D) image.getGraphics();
-        g.drawImage(icon.getImage(), 0, 0, this);
+	private ImageIcon createIcon() {
+		ImageIcon icon = new ImageIcon(getClass().getResource("img/about.gif"));
 
-        String version = App.getVersion();
-        int width = g.getFontMetrics().stringWidth(version);
-        g.setFont(new Font("Sans Serif", Font.PLAIN, 14));
-        g.setColor(Color.black);
-        g.drawString(version, icon.getIconWidth() - width - 10, 182);
+		int width = icon.getIconWidth();
+		int height = icon.getIconHeight();
 
-        icon.setImage(image);
+		BufferedImage image = new BufferedImage(width, height,
+				BufferedImage.TYPE_INT_RGB);
 
-        label.setIcon(icon);
-        add(label);
-    }
+		Graphics2D g = (Graphics2D) image.getGraphics();
 
-    /**
-     * Utility method to show an about panel in a dialog.
-     * 
-     * @param parent
-     *            the frame to use for the dialogs parent
-     */
-    public static void showInDialog(JFrame parent) {
+		g.drawImage(icon.getImage(), 0, 0, this);
 
-        AboutPanel aboutPanel = new AboutPanel();
+		addVersion(g, width, height);
+		addJavaVersion(g, width, height);
+		addOsName(g, width, height);
 
-        JDialog dialog = new JDialog(parent, resources
-                .getString("action.about.description"), true);
-        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-        dialog.setContentPane(aboutPanel);
-        dialog.setResizable(false);
-        dialog.pack();
-        dialog.setLocationRelativeTo(parent);
-        dialog.setVisible(true);
-    }
+		g.dispose();
 
-    private static JWindow splash;
+		icon.setImage(image);
 
-    /**
-     * Utility method to show an about panel in a window.
-     */
-    public static void showSplash() {
+		return icon;
+	}
 
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                AboutPanel aboutPanel = new AboutPanel();
-                aboutPanel.setBorder(BorderFactory.createLineBorder(Color.black, 1));
+	private void addVersion(Graphics2D g, int width, int height) {
 
-                splash = new JWindow();
-                splash.setContentPane(aboutPanel);
-                splash.pack();
-                splash.setLocationRelativeTo(null);
-                splash.setVisible(true);
-            }
-        });
-        
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException ex) {
-        }        
-    }
+		String version = App.getVersion();
 
-    /**
-     * Hide a previously shown splash.
-     * 
-     * @see #showSplash()
-     */
-    public static void hideSplash() {
-        if (splash != null) {
-            SwingUtilities.invokeLater(new Runnable() {
-                public void run() {
-                    splash.dispose();
-                    splash = null;
-                }
-            });
-        }
-    }
+		g.setFont(new Font("Sans Serif", Font.PLAIN, 14));
+		g.setColor(Color.black);
+
+		g.drawString(version, 182, 182);
+	}
+
+	private void addJavaVersion(Graphics2D g, int width, int height) {
+
+		String version = "Java " + new SystemInfo().getJavaVersion();
+
+		g.setFont(new Font("Sans Serif", Font.PLAIN, 12));
+		g.setColor(Color.black);
+
+		g.drawString(version, 5, 242);
+	}
+
+	private void addOsName(Graphics2D g, int width, int height) {
+
+		String version = new SystemInfo().getOsName();
+
+		g.setFont(new Font("Sans Serif", Font.PLAIN, 12));
+		g.setColor(Color.black);
+
+		g.drawString(version, 5, 258);
+	}
+
+	
+	/**
+	 * Utility method to show an about panel in a dialog.
+	 * 
+	 * @param parent
+	 *            the frame to use for the dialogs parent
+	 */
+	public static void showInDialog(JFrame parent) {
+
+		AboutPanel aboutPanel = new AboutPanel();
+
+		JDialog dialog = new JDialog(parent, resources
+				.getString("action.about.description"), true);
+		dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		dialog.setContentPane(aboutPanel);
+		dialog.setResizable(false);
+		dialog.pack();
+		dialog.setLocationRelativeTo(parent);
+		dialog.setVisible(true);
+	}
+
+	private static JWindow splash;
+
+	/**
+	 * Utility method to show an about panel in a window.
+	 */
+	public static void showSplash() {
+
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				AboutPanel aboutPanel = new AboutPanel();
+
+				splash = new JWindow();
+				splash.setContentPane(aboutPanel);
+				splash.pack();
+				splash.setLocationRelativeTo(null);
+				splash.setVisible(true);
+			}
+		});
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException ex) {
+		}
+	}
+
+	/**
+	 * Hide a previously shown splash.
+	 * 
+	 * @see #showSplash()
+	 */
+	public static void hideSplash() {
+		if (splash != null) {
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+					splash.dispose();
+					splash = null;
+				}
+			});
+		}
+	}
 }
