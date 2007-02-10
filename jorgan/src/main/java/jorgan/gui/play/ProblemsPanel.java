d60a3dc0110655bbf92180c3951ed2ab3991157e
Sven Meier <sven@meiers.net>
2007-02-10 18:53:19 +0000

    moved ProblemsPanel and PlayMonitor to new subpackage 'play'

--- jorgan/src/main/java/jorgan/gui/play/ProblemsPanel.java
+++ jorgan/src/main/java/jorgan/gui/play/ProblemsPanel.java
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-package jorgan.gui.construct;
+package jorgan.gui.play;
 
 import java.awt.event.ActionEvent;
 import java.text.MessageFormat;
