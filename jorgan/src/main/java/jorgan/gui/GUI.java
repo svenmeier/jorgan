af697480f40ac0084809d7267ec7c2f7e9d59401
Sven Meier <sven@meiers.net>
2008-03-11 21:05:41 +0000

    tinkering with startup

--- jorgan/src/main/java/jorgan/gui/GUI.java
+++ jorgan/src/main/java/jorgan/gui/GUI.java
@@ -4,6 +4,7 @@ import java.awt.Toolkit;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,12 +21,14 @@ import bias.Configuration;
  */
 public class GUI implements UI {
 
+	public static enum LAF {
+		DEFAULT, SYSTEM, CROSS_PLATFORM
+	}
+	
 	private static Logger log = Logger.getLogger(GUI.class.getName());
 
 	private static Configuration config = Configuration.getRoot()
 			.get(GUI.class);
-
-	private FrameDisposer disposer = new FrameDisposer();
 
 	private boolean showAboutOnStartup = true;
 
@@ -33,110 +36,6 @@ public class GUI implements UI {
 
 	public GUI() {
 		config.read(this);
-	}
-
-	/**
-	 * Start the user interaction.
-	 * 
-	 * @param file
-	 *            optional file that contains an organ
-	 */
-	public void display(final File file) {
-
-		MacAdapter.getInstance().install("jOrgan");
-		
-		if (showAboutOnStartup) {
-			AboutPanel.showSplash();
-		}
-
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				initSwing();
-				showFrame(file);
-			}
-		});
-
-		AboutPanel.hideSplash();
-
-		disposer.waitForDispose();
-	}
-
-	private void showFrame(final File file) {
-		OrganFrame frame = new OrganFrame();
-
-		disposer.attach(frame);
-
-		if (file != null) {
-			frame.openOrgan(file);
-		}
-
-		frame.setVisible(true);
-	}
-
-	private void initSwing() {
-		String plaf = null;
-		try {
-			switch (lookAndFeel) {
-			case DEFAULT:
-				// nothing
-				break;
-			case SYSTEM:
-				plaf = UIManager.getSystemLookAndFeelClassName();
-				break;
-			case CROSS_PLATFORM:
-				plaf = UIManager.getCrossPlatformLookAndFeelClassName();
-				break;
-			}
-
-			if (plaf != null) {
-				log.log(Level.INFO, "setting plaf '" + plaf + "'");
-				UIManager.setLookAndFeel(plaf);
-			}
-		} catch (Exception ex) {
-			log.log(Level.WARNING, "unable to set plaf '" + plaf + "'", ex);
-		}
-
-		Toolkit.getDefaultToolkit().setDynamicLayout(true);
-
-		// IMPORTANT:
-		// Never wait for the result of a spin-over or we'll
-		// run into deadlocks!! (player lock <-> Swing EDT)
-		SpinOverEvaluator.setDefaultWait(false);
-	}
-
-	/**
-	 * The listener that gets attached to the {@link OrganFrame} to dispose it
-	 * on {@link #componentHidden(ComponentEvent)}.
-	 */
-	private class FrameDisposer extends ComponentAdapter {
-
-		private boolean disposed = false;
-
-		private OrganFrame frame;
-
-		private void attach(OrganFrame frame) {
-			frame.addComponentListener(this);
-			this.frame = frame;
-		}
-
-		@Override
-		public synchronized void componentHidden(ComponentEvent e) {
-			frame.dispose();
-
-			disposed = true;
-
-			notify();
-		}
-
-		private synchronized void waitForDispose() {
-			while (!disposed) {
-				try {
-					wait();
-				} catch (InterruptedException ex) {
-					throw new Error("unexpected interruption", ex);
-				}
-			}
-		}
 	}
 
 	public boolean getShowAboutOnStartup() {
@@ -155,7 +54,104 @@ public class GUI implements UI {
 		this.lookAndFeel = lookAndFeel;
 	}
 	
-	public static enum LAF {
-		DEFAULT, SYSTEM, CROSS_PLATFORM
+	/**
+	 * Start the user interaction.
+	 * 
+	 * @param file
+	 *            optional file that contains an organ
+	 */
+	public void display(final File file) {
+
+		MacAdapter.getInstance().install("jOrgan");
+		
+		if (showAboutOnStartup) {
+			AboutPanel.showSplash();
+		}
+
+		Swing swing = new Swing().start(file);
+		
+		AboutPanel.hideSplash();
+		
+		swing.waitForEnd();
+	}
+
+	private class Swing extends ComponentAdapter implements Runnable {
+		
+		private OrganFrame frame;
+		
+		private File file;
+		
+		public Swing start(File file) {
+			try {
+				SwingUtilities.invokeAndWait(this);
+			} catch (InterruptedException e) {
+				throw new Error(e);
+			} catch (InvocationTargetException e) {
+				throw new Error(e);
+			}
+			return this;
+		}
+
+		public void run() {			
+			initSwing();
+			
+			frame = new OrganFrame();
+			frame.addComponentListener(this);
+
+			if (file != null) {
+				frame.openOrgan(file);
+			}
+
+			frame.setVisible(true);
+		}
+		
+		@Override
+		public synchronized void componentHidden(ComponentEvent e) {
+			frame.dispose();
+			frame = null;
+
+			notify();
+		}
+
+		public synchronized void waitForEnd() {
+			while (frame != null) {
+				try {
+					wait();
+				} catch (InterruptedException ex) {
+					throw new Error(ex);
+				}
+			}
+		}
+		
+		private void initSwing() {
+			String plaf = null;
+			try {
+				switch (lookAndFeel) {
+				case DEFAULT:
+					// nothing to do
+					break;
+				case SYSTEM:
+					plaf = UIManager.getSystemLookAndFeelClassName();
+					break;
+				case CROSS_PLATFORM:
+					plaf = UIManager.getCrossPlatformLookAndFeelClassName();
+					break;
+				}
+
+				if (plaf != null) {
+					log.log(Level.INFO, "setting plaf '" + plaf + "'");
+					UIManager.setLookAndFeel(plaf);
+				}
+			} catch (Exception ex) {
+				log.log(Level.WARNING, "unable to set plaf '" + plaf + "'", ex);
+			}
+
+			Toolkit.getDefaultToolkit().setDynamicLayout(true);
+
+			// IMPORTANT:
+			// Never wait for the result of a spin-over or we'll
+			// run into deadlocks!! (player lock <-> Swing EDT)
+			SpinOverEvaluator.setDefaultWait(false);
+		}		
 	}
 }
