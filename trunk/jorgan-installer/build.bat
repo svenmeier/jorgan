7e2c01d8df33543b8009014a25219b32ad8dbb70
svenmeier <svenmeier@c0761391-0d4f-4fe6-b375-9a78db3c6b12>
2009-01-28 21:27:33 +0000

    build jorgan modules automatically

--- trunk/jorgan-installer/build.bat
+++ trunk/jorgan-installer/build.bat
@@ -1,5 +1,13 @@
+set JAVA_HOME="C:\Programme\Java\jdk1.6.0_10"
+set ANT="C:\Programme\apache-ant-1.7.1\bin\ant.bat"
 set LAUNCH4J="C:\Programme\Launch4j\launch4jc.exe"
 set INNO_SETUP="C:\Programme\Inno Setup 5\ISCC.exe"
+
+call %ANT% -buildfile ../core/build.xml
+call %ANT% -buildfile ../creative/build.xml
+call %ANT% -buildfile ../fluidsynth/build.xml
+call %ANT% -buildfile ../linuxsampler/build.xml
+call %ANT% -buildfile ../soundfont/build.xml
 
 :Clean
 rmdir /S /Q target
