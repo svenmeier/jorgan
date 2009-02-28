229e8ac8f1130bf81f620f4cc14ebe371fe2c8a0
nmeier <nmeier@c0761391-0d4f-4fe6-b375-9a78db3c6b12>
2009-02-28 18:29:33 +0000

    new version with improved layout and interactivity

--- trunk/jorgan-graph/src/main/java/jorgan/graph/gui/dock/StructureDockable.java
+++ trunk/jorgan-graph/src/main/java/jorgan/graph/gui/dock/StructureDockable.java
@@ -7,12 +7,16 @@ import gj.layout.hierarchical.HierarchicalLayoutAlgorithm;
 import gj.model.Edge;
 import gj.model.Graph;
 import gj.model.Vertex;
+import gj.ui.DefaultGraphRenderer;
 import gj.ui.GraphWidget;
 import gj.util.DefaultEdge;
 import gj.util.DefaultVertex;
 import gj.util.EmptyGraph;
 
-import java.awt.Shape;
+import java.awt.Color;
+import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -27,7 +31,10 @@ import jorgan.disposition.Message;
 import jorgan.disposition.Reference;
 import jorgan.disposition.event.OrganListener;
 import jorgan.gui.dock.OrganDockable;
+import jorgan.session.ElementSelection;
 import jorgan.session.OrganSession;
+import jorgan.session.event.ElementSelectionEvent;
+import jorgan.session.event.ElementSelectionListener;
 import bias.Configuration;
 
 /**
@@ -37,8 +44,9 @@ public class StructureDockable extends OrganDockable {
   
   private static Configuration config = Configuration.getRoot().get(StructureDockable.class);
   private GraphWidget graphWidget;
-  private OrganListenerImpl organListener = new OrganListenerImpl(); 
+  private ListenerImpl listener = new ListenerImpl(); 
   private OrganSession session = null;
+  private Element selection;
   
   /**
    * Constructor
@@ -47,6 +55,18 @@ public class StructureDockable extends OrganDockable {
     config.read(this);
     
     graphWidget = new GraphWidget();
+    graphWidget.setRenderer(new DefaultGraphRenderer() {
+      @Override
+      @SuppressWarnings({ "unchecked" })
+      protected Color getColor(Vertex vertex) {
+        if (selection!=null && ((DefaultVertex<Element>)vertex).getContent().equals(selection))
+          return Color.BLUE;
+        return Color.BLACK;
+      }
+    });
+    
+    graphWidget.addMouseListener(listener);
+    
     setContent(new JScrollPane(graphWidget));
   }
   
@@ -65,31 +85,35 @@ public class StructureDockable extends OrganDockable {
     // TODO this is kinda bad to redo all over every time :)
     ElementGraph graph = new ElementGraph(session.getOrgan().getElements());
     Layout2D layout = new DefaultLayout(new Rectangle2D.Double(-20,-20,40,40));
-    Shape shape = new Rectangle2D.Double();
+    Rectangle bounds = new Rectangle();
     try {
       HierarchicalLayoutAlgorithm a = new HierarchicalLayoutAlgorithm();
-      a.setDistanceBetweenLayers(80);
-      a.setDistanceBetweenVertices(80);
-      shape = a.apply(graph, layout, null, null);
+      a.setDistanceBetweenLayers(30);
+      a.setDistanceBetweenVertices(30);
+      bounds = a.apply(graph, layout, null, null).getBounds();
     } catch (LayoutAlgorithmException e) {
       // FIXME report something
       e.printStackTrace();
     }
     graphWidget.setGraphLayout(layout);
-    graphWidget.setGraph(graph, shape.getBounds());
+    graphWidget.setGraph(graph, bounds);
     
   }
 
   @Override
   public void setSession(OrganSession session) {
 
-    if (this.session!=null)
-      this.session.removeOrganListener(organListener);
+    if (this.session!=null) {
+      this.session.removeOrganListener(listener);
+      this.session.addSelectionListener(listener);
+    }
     
     this.session = session;
 
-    if (this.session!=null)
-      this.session.addOrganListener(organListener);
+    if (this.session!=null) {
+      this.session.addOrganListener(listener);
+      this.session.addSelectionListener(listener);
+    }
     
     rebuild();
   }
@@ -136,10 +160,16 @@ public class StructureDockable extends OrganDockable {
   } //ElementGraph
   
   /**
-   * our core listener
+   * our listener for organ events
    */
-  private class OrganListenerImpl implements OrganListener {
+  private class ListenerImpl implements MouseListener, OrganListener, ElementSelectionListener {
 
+    public void selectionChanged(ElementSelectionEvent ev) {
+      ElementSelection es = session.getElementSelection();
+      selection = es.getSelectedElement();
+      graphWidget.repaint();
+    }
+    
     public void elementAdded(Element element) {
       rebuild();
     }
@@ -171,6 +201,25 @@ public class StructureDockable extends OrganDockable {
 
     public void referenceRemoved(Element element, Reference<?> reference) {
       rebuild();
+    }
+
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public void mouseExited(MouseEvent e) {
+    }
+
+    @SuppressWarnings({ "unchecked" })
+    public void mousePressed(MouseEvent e) {
+      DefaultVertex<Element> vertex = (DefaultVertex<Element>)graphWidget.getVertexAt(e.getPoint());
+      if (vertex!=null)
+        session.getElementSelection().setSelectedElement(vertex.getContent());
+    }
+
+    public void mouseReleased(MouseEvent e) {
     }
     
   } //OrganCallback
