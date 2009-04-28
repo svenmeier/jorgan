4f9c551b3e12e4d6650241cc7afca57dc268c6c0
svenmeier <svenmeier@c0761391-0d4f-4fe6-b375-9a78db3c6b12>
2009-04-28 16:49:38 +0000

    multiple track support

--- trunk/jorgan-recorder/src/main/java/jorgan/recorder/swing/LabelPanel.java
+++ trunk/jorgan-recorder/src/main/java/jorgan/recorder/swing/LabelPanel.java
@@ -1,5 +1,24 @@
+/*
+ * jOrgan - Java Virtual Organ
+ * Copyright (C) 2003 Sven Meier
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package jorgan.recorder.swing;
 
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
@@ -11,11 +30,16 @@ import javax.swing.JPanel;
 public abstract class LabelPanel extends JPanel {
 
 	public LabelPanel() {
-		setFont(new Font("Monospaced", Font.PLAIN, 16));
+		setFont(new Font("Monospaced", Font.PLAIN, 24));
 	}
 
 	protected abstract String getText();
 
+	@Override
+	public Dimension getPreferredSize() {
+		return new Dimension(0, getFont().getSize());
+	}
+	
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
