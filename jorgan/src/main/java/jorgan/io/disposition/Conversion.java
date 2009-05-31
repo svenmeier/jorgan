fcfa2140a61d73b2462ac92c344e80c1c8dae478
Sven Meier <sven@meiers.net>
2009-05-31 14:48:15 +0000

    moved conversion, version now parsed as attribute

--- jorgan/src/main/java/jorgan/io/disposition/Conversion.java
+++ jorgan/src/main/java/jorgan/io/disposition/Conversion.java
@@ -18,14 +18,10 @@
  */
 package jorgan.io.disposition;
 
-import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.xml.transform.OutputKeys;
@@ -37,119 +33,65 @@ import javax.xml.transform.stream.StreamSource;
 
 public class Conversion {
 
-	private static final Logger logger = Logger.getLogger(Conversion.class
-			.getName());
-
-	private static Conversion[] conversions = new Conversion[] {
-			new Conversion("<organ>", "convert1.0To2.0-beta.xsl"),
-			new Conversion("<organ *version=\"(2\\.0-beta.*|2\\.0-RC1)\" *>",
-					"convert2.0-betaTo2.0.xsl"),
-			new Conversion("<organ *version=\"2\\.0\" *>",
-					"convert2.0To2.1-beta.xsl"),
-			new Conversion("<organ *version=\"2\\.1-beta.*\" *>",
-					"convert2.1-betaTo2.1.xsl"),
-			new Conversion("<organ *version=\"2\\.1.*\" *>",
-					"convert2.1To2.2-beta.xsl"),
-			new Conversion("<organ *version=\"2\\.2-beta.*\" *>",
-					"convert2.2-betaTo2.2.xsl"),
-			new Conversion("<organ *version=\"2\\.2.*\" *>",
-					"convert2.2To2.3-beta.xsl"),
-			new Conversion("<organ *version=\"2\\.3-beta.*\" *>",
-					"convert2.3-betaTo2.3.xsl"),
-			new Conversion("<organ *version=\"2\\.3.*\" *>",
-					"convert2.3To2.4-beta.xsl"),
-			new Conversion("<organ *version=\"2\\.4-beta.*\" *>",
-					"convert2.4-betaTo2.4.xsl"),
-			new Conversion("<organ *version=\"2\\.4.*\" *>",
-					"convert2.4To3.0-beta.xsl"),
-			new Conversion("<organ *version=\"3\\.0-beta.*\" *>",
-					"convert3.0-betaTo3.0.xsl"),
-			new Conversion("<organ *version=\"3\\.0\" *>",
-					"convert3.0To3.1.xsl"),
-			new Conversion("<organ *version=\"3\\.[1|2].*\" *>",
-					"convert3.1To3.3.xsl"),
-			new Conversion("<organ *version=\"3\\.[3|4].*\" *>",
-					"convert3.3To3.5-beta.xsl"),
-			new Conversion("<organ *version=\"3\\.5-beta.*\" *>",
-					"convert3.5-betaTo3.5.xsl"),
-			new Conversion("<organ *version=\"3\\.5\" *>",
-					"convert3.5To3.5.1.xsl") };
-
-	private String pattern;
+	public static Conversion[] list = new Conversion[] {
+		new Conversion("2\\.0-.*", "convert2.0-betaTo2.0.xsl"),
+		new Conversion("2\\.0", "convert2.0To2.1-beta.xsl"),
+		new Conversion("2\\.1-beta.*", "convert2.1-betaTo2.1.xsl"),
+		new Conversion("2\\.1.*", "convert2.1To2.2-beta.xsl"),
+		new Conversion("2\\.2-beta.*", "convert2.2-betaTo2.2.xsl"),
+		new Conversion("2\\.2.*", "convert2.2To2.3-beta.xsl"),
+		new Conversion("2\\.3-beta.*", "convert2.3-betaTo2.3.xsl"),
+		new Conversion("2\\.3.*", "convert2.3To2.4-beta.xsl"),
+		new Conversion("2\\.4-beta.*", "convert2.4-betaTo2.4.xsl"),
+		new Conversion("2\\.4.*", "convert2.4To3.0-beta.xsl"),
+		new Conversion("3\\.0-beta.*", "convert3.0-betaTo3.0.xsl"),
+		new Conversion("3\\.0", "convert3.0To3.1.xsl"),
+		new Conversion("3\\.[1|2].*", "convert3.1To3.3.xsl"),
+		new Conversion("3\\.[3|4].*", "convert3.3To3.5-beta.xsl"),
+		new Conversion("3\\.5-beta.*", "convert3.5-betaTo3.5.xsl"),
+		new Conversion("3\\.5", "convert3.5To3.5.1.xsl"),
+		new Conversion("3\\.[5|6].*", "convert3.6To3.7.xsl") };
+	
+	private Pattern pattern;
 
 	private String xsl;
 
 	public Conversion(String pattern, String xsl) {
-		this.pattern = pattern;
+		this.pattern = Pattern.compile(pattern);
 		this.xsl = xsl;
 	}
 
 	public boolean isApplicable(String header) {
-		Pattern pattern = Pattern.compile(this.pattern);
-		Matcher matcher = pattern.matcher(header);
-
-		return matcher.find();
+		return pattern.matcher(header).matches();
 	}
 
-	public InputStream convert(InputStream in) throws TransformerException,
-			IOException {
+	@Override
+	public String toString() {
+		return xsl;
+	}
+	
+	public InputStream convert(InputStream in) throws IOException {
 		TransformerFactory factory = TransformerFactory.newInstance();
 		factory.setAttribute("indent-number", new Integer(4));
 
-		Transformer transform = factory.newTransformer(new StreamSource(
-				Conversion.class.getResourceAsStream(xsl)));
+		Transformer transform;
+		try {
+			transform = factory.newTransformer(new StreamSource(
+					Conversion.class.getResourceAsStream("conversion/" + xsl)));
 
-		transform.setOutputProperty(OutputKeys.INDENT, "yes");
+			transform.setOutputProperty(OutputKeys.INDENT, "yes");
 
-		File temp = File.createTempFile(xsl + ".", ".xml");
+			File temp = File.createTempFile(xsl + ".", ".xml");
 
-		transform.transform(new StreamSource(in), new StreamResult(temp));
+			transform.transform(new StreamSource(in), new StreamResult(temp));
 
-		in.close();
+			in.close();
 
-		return new FileInputStream(temp);
-	}
-
-	public static InputStream convertAll(InputStream in)
-			throws TransformerException, IOException {
-
-		in = new BufferedInputStream(in);
-
-		String header = getHeader(in);
-
-		int index = 0;
-		while (index < conversions.length) {
-			if (conversions[index].isApplicable(header)) {
-				logger.log(Level.INFO, "applicable '"
-						+ conversions[index].pattern + "'");
-				break;
-			}
-			index++;
+			return new FileInputStream(temp);
+		} catch (TransformerException e) {
+			IOException ex = new IOException();
+			ex.initCause(e);
+			throw ex;
 		}
-
-		while (index < conversions.length) {
-			logger.log(Level.INFO, "applying '" + conversions[index].xsl + "'");
-
-			in = conversions[index].convert(in);
-			index++;
-		}
-
-		return in;
-	}
-
-	private static String getHeader(InputStream in) throws IOException {
-		in.mark(2048);
-
-		byte[] bytes = new byte[1024];
-		int offset = 0;
-		while (offset != -1 && offset < bytes.length) {
-			offset = in.read(bytes, offset, bytes.length - offset);
-		}
-
-		in.reset();
-
-		String header = new String(bytes, "UTF-8");
-
-		return header;
 	}
 }
