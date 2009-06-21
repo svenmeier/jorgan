2c6171e50cc7393facfeabab5cc8f819dd657cc8
svenmeier <svenmeier@c0761391-0d4f-4fe6-b375-9a78db3c6b12>
2009-06-21 14:32:59 +0000

    added new projects

--- trunk/jorgan-installer/build.bat
+++ trunk/jorgan-installer/build.bat
@@ -4,12 +4,16 @@ set LAUNCH4J="C:\Programme\Launch4j\launch4jc.exe"
 set INNO_SETUP="C:\Programme\Inno Setup 5\ISCC.exe"
 
 call %ANT% -buildfile ../core/build.xml
-call %ANT% -buildfile ../customizer/build.xml
-call %ANT% -buildfile ../importer/build.xml
-call %ANT% -buildfile ../recorder/build.xml
 call %ANT% -buildfile ../creative/build.xml
+call %ANT% -buildfile ../customizer/build.xml
 call %ANT% -buildfile ../fluidsynth/build.xml
+call %ANT% -buildfile ../importer/build.xml
+call %ANT% -buildfile ../installer/build.xml
+call %ANT% -buildfile ../keyboard/build.xml
 call %ANT% -buildfile ../linuxsampler/build.xml
+call %ANT% -buildfile ../midimerger/build.xml
+call %ANT% -buildfile ../recorder/build.xml
+call %ANT% -buildfile ../skins/build.xml
 call %ANT% -buildfile ../soundfont/build.xml
 
 :Clean
