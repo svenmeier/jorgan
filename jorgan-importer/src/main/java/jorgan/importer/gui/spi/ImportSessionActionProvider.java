85bd30a65e5ecd660fa2824ab919177cccbe3da0
Sven Meier <sven@meiers.net>
2009-06-21 13:42:21 +0000

    merged first beta

--- jorgan-importer/src/main/java/jorgan/importer/gui/spi/ImportSessionActionProvider.java
+++ jorgan-importer/src/main/java/jorgan/importer/gui/spi/ImportSessionActionProvider.java
@@ -6,16 +6,17 @@ import java.util.List;
 
 import javax.swing.Action;
 
+import jorgan.gui.OrganFrame;
 import jorgan.gui.spi.SessionActionProvider;
 import jorgan.importer.gui.ImportAction;
 import jorgan.session.OrganSession;
 
 public class ImportSessionActionProvider implements SessionActionProvider {
 
-	public List<Action> getActions(OrganSession session) {
+	public List<Action> getActions(OrganSession session, OrganFrame frame) {
 		List<Action> actions = new ArrayList<Action>();
 
-		actions.add(new ImportAction(session));
+		actions.add(new ImportAction(session, frame));
 
 		return actions;
 	}
