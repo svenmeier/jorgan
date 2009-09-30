4fb52ab304905964a7de15a752de7135d8906f0e
svenmeier <svenmeier@c0761391-0d4f-4fe6-b375-9a78db3c6b12>
2009-09-30 18:38:25 +0000

    using standard conversion

--- trunk/jorgan-memory/src/main/java/jorgan/memory/util/Extract.java
+++ trunk/jorgan-memory/src/main/java/jorgan/memory/util/Extract.java
@@ -1,5 +1,6 @@
 package jorgan.memory.util;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -14,6 +15,12 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
+import jorgan.gui.file.DispositionFileFilter;
+import jorgan.io.DispositionStream;
+import jorgan.io.disposition.Conversion;
+
+import com.thoughtworks.xstream.converters.ConversionException;
+
 public class Extract {
 
 	private TransformerFactory factory = TransformerFactory.newInstance();
@@ -21,7 +28,30 @@ public class Extract {
 	public Extract() {
 	}
 
-	public void convert(InputStream in, OutputStream out) throws IOException {
+	private InputStream convert(InputStream in) throws ConversionException,
+			IOException {
+
+		in = new BufferedInputStream(in);
+
+		String version = DispositionStream.getVersion(in);
+
+		boolean apply = false;
+		for (Conversion conversion : Conversion.list) {
+			if (conversion.getPattern().startsWith("3\\.7")) {
+				break;
+			}
+
+			if (apply || conversion.isApplicable(version)) {
+				apply = true;
+
+				in = conversion.convert(in);
+			}
+		}
+
+		return in;
+	}
+
+	public void extract(InputStream in, OutputStream out) throws IOException {
 
 		Transformer transform;
 		try {
@@ -43,7 +73,9 @@ public class Extract {
 		OutputStream out = new FileOutputStream(to);
 
 		try {
-			convert(in, out);
+			in = convert(in);
+
+			extract(in, out);
 		} finally {
 			in.close();
 			out.close();
@@ -51,14 +83,30 @@ public class Extract {
 	}
 
 	public static void main(String[] args) {
-		if (args.length != 2) {
-			System.out.println("Supply source disposition and target memory file");
+		if (args.length < 1 || args.length > 2) {
+			System.out
+					.println("Supply source disposition and target memory file");
 			System.exit(1);
 			return;
 		}
-		
+
+		File in = new File(args[0]);
+		File out;
+		if (args.length == 1) {
+			String name;
+			if (args[0].endsWith(DispositionFileFilter.FILE_SUFFIX)) {
+				name = args[0].substring(0, args[0].length()
+						- DispositionFileFilter.FILE_SUFFIX.length());
+			} else {
+				name = args[0];
+			}
+			out = new File(name + ".memory");
+		} else {
+			out = new File(args[1]);
+		}
+
 		try {
-			new Extract().now(new File(args[0]), new File(args[1]));
+			new Extract().now(in, out);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- trunk/jorgan-memory/src/main/java/jorgan/memory/util/extract.xsl
+++ trunk/jorgan-memory/src/main/java/jorgan/memory/util/extract.xsl
@@ -1,8 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" >
 	<xsl:template match="/">
-      	<xsl:for-each select="//memory">
-			<memory>
+		<memory>
+    	  	<xsl:for-each select="//memory">
 				<titles>
 			      	<xsl:for-each select="titles/string">
 			      		<string>
@@ -14,14 +14,7 @@
 				<combinations>
 			      	<xsl:for-each select="references/reference">
 						<xsl:variable name="id">
-				      		<xsl:choose>
-				      			<xsl:when test="element/@ref">
-									<xsl:value-of select="element/@ref"/>
-				      			</xsl:when>
-				      			<xsl:otherwise>
-									<xsl:value-of select="@id"/>
-				      			</xsl:otherwise>
-				      		</xsl:choose>			      	
+							<xsl:value-of select="element/@ref"/>
 						</xsl:variable>
 						
 			      		<combination>
@@ -33,7 +26,7 @@
 						      	<xsl:for-each select="//combination[@id = $id]/references/combination-switchReference">
 						      		<switch>
 										<xsl:attribute name="id">
-											<xsl:value-of select="translate(@id, '-', '0')"/>
+											<xsl:value-of select="element/@ref"/>
 										</xsl:attribute>
 										<actives>
 											<xsl:value-of select="actives"/>
@@ -44,7 +37,7 @@
 						      	<xsl:for-each select="//combination[@id = $id]/references/combination-continuousReference">
 						      		<continuous>
 										<xsl:attribute name="id">
-											<xsl:value-of select="translate(@id, '-', '0')"/>
+											<xsl:value-of select="element/@ref"/>
 										</xsl:attribute>
 										<values>
 											<xsl:value-of select="values"/>
@@ -55,7 +48,7 @@
 			      		</combination>
 			      	</xsl:for-each>
 				</combinations>
-			</memory>
-      	</xsl:for-each>
+	      	</xsl:for-each>
+		</memory>
 	</xsl:template>
  </xsl:stylesheet>
