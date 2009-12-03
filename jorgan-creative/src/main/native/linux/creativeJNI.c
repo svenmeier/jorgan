542629859cda486eddf31c816f35440172087919
Sven Meier <sven@meiers.net>
2009-12-03 21:00:14 +0000

    creative on linux

--- jorgan-creative/src/main/native/linux/creativeJNI.c
+++ jorgan-creative/src/main/native/linux/creativeJNI.c
@@ -3,74 +3,108 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <alsa/asoundlib.h>
-#include <awebank.h>
-#include <sfopts.h>
+#include "awebank.h"
+#include "sfopts.h"
+#include "seq.h"
 #include "jorgan.creative.SoundFontManager.h"
 
-  /**
-   * Class:     jorgan_creative_SoundFontManager
-   * Method:    getNumDevices
-   * Signature: ()I
-   */
-  JNIEXPORT jint JNICALL Java_jorgan_creative_SoundFontManager_getNumDevices (JNIEnv *env, jobject obj) {
+static jfieldID fieldID;
+
+static AWEOps load_ops = {
+	seq_load_patch,
+	seq_mem_avail,
+	seq_reset_samples,
+	seq_remove_samples,
+	seq_zero_atten
+};
+
+extern sf_options awe_option;
+
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
+  
+  JNIEnv *env;
+  
+  if ((*vm)->GetEnv(vm, (void **)&env, JNI_VERSION_1_2)) {
+    return JNI_ERR;
+  }
+
+  jclass clazz = (*env)->FindClass(env, "jorgan/creative/SoundFontManager");
+
+  fieldID = (*env)->GetFieldID(env, clazz, "deviceName", "Ljava/lang/String;");
+  if (fieldID == NULL) {
+    return JNI_ERR;
+  }
+
+  return JNI_VERSION_1_2;
+}
+
+JNIEXPORT void JNICALL JNI_OnUnload(JavaVM *vm, void *reserved) {
+}
+
+/**
+ * Helper for throwing an Exception
+ */
+void throwException(JNIEnv* env, char* exception, char* pattern, ...) {
+
+  // Assemble message from pattern and variable number of parameters
+  va_list args;
+  va_start(args, pattern);
+  char msg[256];
+  vsnprintf(msg, 256, pattern, args);
+  va_end(args);
+
+  // create the IOException  
+  jclass ioex = (*env)->FindClass(env, exception);
+  (*env)->ThrowNew(env, ioex, msg);
+}
+
+jstring getDeviceName(JNIEnv *env, jobject obj) {
+  // TODO strip anything outside []
+  return (*env)->GetObjectField(env, obj, fieldID);
+}
+
+JNIEXPORT void JNICALL Java_jorgan_creative_SoundFontManager_init(JNIEnv *env, jobject obj) {
+  // TODO
+}
+
+JNIEXPORT void JNICALL Java_jorgan_creative_SoundFontManager_clear(JNIEnv *env, jobject obj, jint bank) {
+  jstring deviceName = getDeviceName(env, obj);
+  char* cDeviceName = (*env)->GetStringUTFChars(env, deviceName, NULL);
+  seq_alsa_init(cDeviceName);
+
+  //awe_option.default_bank = bank;
+  seq_remove_samples();
+
+  seq_alsa_end();
+  (*env)->ReleaseStringUTFChars(env, deviceName, cDeviceName);
+}
+
+JNIEXPORT jboolean JNICALL Java_jorgan_creative_SoundFontManager_isLoaded(JNIEnv *env, jobject obj, jint bank) {
+  // TODO
+  return JNI_FALSE;
+}
+
+JNIEXPORT void JNICALL Java_jorgan_creative_SoundFontManager_load(JNIEnv *env, jobject obj, jint bank, jstring fileName) {
+  jstring deviceName = getDeviceName(env, obj);
+  //char* cDeviceName = (*env)->GetStringUTFChars(env, deviceName, NULL);
+  char* cDeviceName = "hw:0,2";
+  seq_alsa_init(cDeviceName);
+
+  char* cfileName = (*env)->GetStringUTFChars(env, fileName, NULL);
+  //awe_option.default_bank = bank;
+  awe_load_bank(&load_ops, cfileName, NULL, 0);
+  (*env)->ReleaseStringUTFChars(env, fileName, cfileName);
+
+  seq_alsa_end();
+  //(*env)->ReleaseStringUTFChars(env, deviceName, cDeviceName);
+}
+
+JNIEXPORT jstring JNICALL Java_jorgan_creative_SoundFontManager_getDescriptor(JNIEnv *env, jobject obj, jint bank) {
+  // TODO
+  return NULL;
+}
 	
-    return 0;
-  }
-
-  /**
-   * Class:     jorgan_creative_SoundFontManager
-   * Method:    getDeviceName
-   * Signature: (I)Ljava/lang/String;
-   */
-  JNIEXPORT jstring JNICALL Java_jorgan_creative_SoundFontManager_getDeviceName(JNIEnv *env, jobject obj, jint device) {
-
-    return 0;
-  }
-	
-  /**
-   * Class:     jorgan_creative_SoundFontManager
-   * Method:    isBankUsed
-   * Signature: (II)Z
-   */
-  JNIEXPORT jboolean JNICALL Java_jorgan_creative_SoundFontManager_isBankUsed(JNIEnv *env, jobject obj, jint device, jint bank) {
-
-    return JNI_FALSE;
-  }
-
-  /**
-   * Class:     jorgan_creative_SoundFontManager
-   * Method:    getBankDescriptor
-   * Signature: (II)Ljava/lang/String;
-   */
-  JNIEXPORT jstring JNICALL Java_jorgan_creative_SoundFontManager_getBankDescriptor(JNIEnv *env, jobject obj, jint device, jint bank) {
-	
-    return 0;
-  }
-	
-  /**
-   * Class:     jorgan_creative_SoundFontManager
-   * Method:    loadBank
-   * Signature: (IILjava/lang/String;)V
-   */
-  JNIEXPORT void JNICALL Java_jorgan_creative_SoundFontManager_loadBank(JNIEnv *env, jobject obj, jint device, jint bank, jstring jfile) {
-
-  }
-
-  /**
-   * Class:     jorgan_creative_SoundFontManager
-   * Method:    clearBank
-   * Signature: (II)V
-   */
-  JNIEXPORT void JNICALL Java_jorgan_creative_SoundFontManager_clearBank(JNIEnv *env, jobject obj, jint device, jint bank) {
-	
-  }
-
-  /**
-   * Class:     jorgan_creative_SoundFontManager
-   * Method:    getPresetDescriptor
-   * Signature: (III)Ljava/lang/String;
-   */
-  JNIEXPORT jstring JNICALL Java_jorgan_creative_SoundFontManager_getPresetDescriptor(JNIEnv *env, jobject obj, jint device, jint bank, jint program) {
-
-    return 0;
-  }
+JNIEXPORT jstring JNICALL Java_jorgan_creative_SoundFontManager_getPresetDescriptor(JNIEnv *env, jobject obj, jint bank, jint preset) {
+  // TODO
+  return NULL;
+}
--- jorgan-creative/src/test/java/jorgan/creative/SoundFontManagerTest.java
+++ jorgan-creative/src/test/java/jorgan/creative/SoundFontManagerTest.java
@@ -2,6 +2,8 @@ package jorgan.creative;
 
 import java.io.File;
 
+import jorgan.midi.DevicePool;
+import jorgan.midi.Direction;
 import junit.framework.TestCase;
 
 /**
@@ -16,36 +18,24 @@ public class SoundFontManagerTest extends TestCase {
 
 	public void test() throws Exception {
 
-		// Access it
-		String[] deviceNames = SoundFontManager.getDeviceNames();
-		System.out.println("Number of Devices: " + deviceNames.length);
-
-		// Loop over devices
-		for (String deviceName : deviceNames) {
-			// get its name
-			System.out.println("Device : " + deviceName);
-			// loop through its banks
-			for (int j = 0; j <= 127; j++) {
-				SoundFontManager manager = new SoundFontManager(deviceName, j);
-				if (manager.isLoaded()) {
-					System.out.println("Bank #" + j + ": "
-							+ manager.getDescriptor());
-				}
+		SoundFontManager manager = null;
+		
+		for (String deviceName : DevicePool.instance().getMidiDeviceNames(Direction.OUT)) {
+			try {
+				manager = new SoundFontManager(deviceName);
+			} catch (IllegalArgumentException noCreativeDevice) {
 			}
 		}
+		
+		int bank = 25;
 
-		SoundFontManager manager = new SoundFontManager(deviceNames[0], 25);
+		manager.load(bank, new File("./src/main/dispositions/creative-example.SF2"));
 
-		// Try to load a font
-		manager.load(new File("./src/main/dispositions/creative-example.SF2"));
-
-		// loop through its programs
 		for (int p = 0; p < 128; p++) {
 			System.out.println("Program #" + p + ": "
-					+ manager.getPresetDescriptor(p));
+					+ manager.getPresetDescriptor(bank, p));
 		}
 
-		// Clear
-		manager.clear();
+		manager.clear(bank);
 	}
 }
