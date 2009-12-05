e90371ab6cb466d14acc5634bbb39a76fbab385e
svenmeier <svenmeier@c0761391-0d4f-4fe6-b375-9a78db3c6b12>
2009-12-05 08:29:02 +0000

    new jni extension for common native code

--- trunk/jorgan-creative/src/main/native/linux/creativeJNI.c
+++ trunk/jorgan-creative/src/main/native/linux/creativeJNI.c
@@ -3,12 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <alsa/asoundlib.h>
+#include "exception.h"
 #include "awebank.h"
 #include "sfopts.h"
 #include "seq.h"
 #include "jorgan.creative.SoundFontManager.h"
-
-static jfieldID fieldID;
 
 static AWEOps load_ops = {
 	seq_load_patch,
@@ -20,91 +19,88 @@ static AWEOps load_ops = {
 
 extern sf_options awe_option;
 
-JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
-  
-  JNIEnv *env;
-  
-  if ((*vm)->GetEnv(vm, (void **)&env, JNI_VERSION_1_2)) {
-    return JNI_ERR;
-  }
+typedef struct _Context {
+	char* deviceName;
+} Context;
 
-  jclass clazz = (*env)->FindClass(env, "jorgan/creative/SoundFontManager");
-
-  fieldID = (*env)->GetFieldID(env, clazz, "deviceName", "Ljava/lang/String;");
-  if (fieldID == NULL) {
-    return JNI_ERR;
-  }
-
-  return JNI_VERSION_1_2;
+Context* createContext() {
+	return (Context*) malloc(sizeof(Context));
 }
 
-JNIEXPORT void JNICALL JNI_OnUnload(JavaVM *vm, void *reserved) {
+void destroyContext(Context* context) {
+	free(context->deviceName);
+
+	free(context);
 }
 
-/**
- * Helper for throwing an Exception
- */
-void throwException(JNIEnv* env, char* exception, char* pattern, ...) {
+JNIEXPORT
+jobject JNICALL Java_jorgan_creative_SoundFontManager_init(JNIEnv* env, jclass jclass, jstring jdeviceName) {
+	Context* context = createContext();
 
-  // Assemble message from pattern and variable number of parameters
-  va_list args;
-  va_start(args, pattern);
-  char msg[256];
-  vsnprintf(msg, 256, pattern, args);
-  va_end(args);
+	const char* deviceName = (char*) (*env)->GetStringUTFChars(env, jdeviceName, NULL);
 
-  // create the IOException  
-  jclass ioex = (*env)->FindClass(env, exception);
-  (*env)->ThrowNew(env, ioex, msg);
+	char* leftBrace = strchr(deviceName, '[');
+	if (leftBrace == NULL) {
+		leftBrace = (char*)deviceName;
+	} else {
+		leftBrace++;
+	}
+	char* rightBrace = strchr(deviceName, ']');
+	if (rightBrace == NULL) {
+		rightBrace = (char*)deviceName;
+	}
+	context->deviceName = (char*)calloc(strlen(deviceName) + 1, sizeof(char));
+	strncpy(context->deviceName, leftBrace, rightBrace - leftBrace);
+
+	(*env)->ReleaseStringUTFChars(env, jdeviceName, deviceName);
+
+	return (*env)->NewDirectByteBuffer(env, (void*) context, sizeof(Context));
 }
 
-jstring getDeviceName(JNIEnv *env, jobject obj) {
-  // TODO strip anything outside []
-  return (*env)->GetObjectField(env, obj, fieldID);
+JNIEXPORT
+void JNICALL Java_jorgan_creative_SoundFontManager_destroy(JNIEnv* env, jclass jclass, jobject jcontext) {
+	Context* context = (Context*) (*env)->GetDirectBufferAddress(env, jcontext);
+
+	destroyContext(context);
 }
 
-JNIEXPORT void JNICALL Java_jorgan_creative_SoundFontManager_init(JNIEnv *env, jobject obj) {
-  // TODO
+JNIEXPORT void JNICALL Java_jorgan_creative_SoundFontManager_clear(JNIEnv* env, jclass jclass, jobject jcontext, jint jbank) {
+	Context* context = (Context*) (*env)->GetDirectBufferAddress(env, jcontext);
+
+	seq_alsa_init(context->deviceName);
+
+	//awe_option.default_bank = bank;
+
+	seq_remove_samples();
+
+	seq_alsa_end();
 }
 
-JNIEXPORT void JNICALL Java_jorgan_creative_SoundFontManager_clear(JNIEnv *env, jobject obj, jint bank) {
-  jstring deviceName = getDeviceName(env, obj);
-  char* cDeviceName = (*env)->GetStringUTFChars(env, deviceName, NULL);
-  seq_alsa_init(cDeviceName);
-
-  //awe_option.default_bank = bank;
-  seq_remove_samples();
-
-  seq_alsa_end();
-  (*env)->ReleaseStringUTFChars(env, deviceName, cDeviceName);
+JNIEXPORT jboolean JNICALL Java_jorgan_creative_SoundFontManager_isLoaded(JNIEnv *env, jclass jclass, jobject jcontext, jint jbank) {
+	// not implemented
+	return JNI_FALSE;
 }
 
-JNIEXPORT jboolean JNICALL Java_jorgan_creative_SoundFontManager_isLoaded(JNIEnv *env, jobject obj, jint bank) {
-  // TODO
-  return JNI_FALSE;
+JNIEXPORT void JNICALL Java_jorgan_creative_SoundFontManager_load(JNIEnv *env, jclass jclass, jobject jcontext, jint jbank, jstring jfileName) {
+	Context* context = (Context*) (*env)->GetDirectBufferAddress(env, jcontext);
+
+	seq_alsa_init(context->deviceName);
+
+	//awe_option.default_bank = bank;
+
+	char* fileName = (char*) (*env)->GetStringUTFChars(env, jfileName, NULL);
+	awe_load_bank(&load_ops, fileName, NULL, 0);
+	(*env)->ReleaseStringUTFChars(env, jfileName, fileName);
+
+	seq_alsa_end();
 }
 
-JNIEXPORT void JNICALL Java_jorgan_creative_SoundFontManager_load(JNIEnv *env, jobject obj, jint bank, jstring fileName) {
-  jstring deviceName = getDeviceName(env, obj);
-  //char* cDeviceName = (*env)->GetStringUTFChars(env, deviceName, NULL);
-  char* cDeviceName = "hw:0,2";
-  seq_alsa_init(cDeviceName);
-
-  char* cfileName = (*env)->GetStringUTFChars(env, fileName, NULL);
-  //awe_option.default_bank = bank;
-  awe_load_bank(&load_ops, cfileName, NULL, 0);
-  (*env)->ReleaseStringUTFChars(env, fileName, cfileName);
-
-  seq_alsa_end();
-  //(*env)->ReleaseStringUTFChars(env, deviceName, cDeviceName);
-}
-
-JNIEXPORT jstring JNICALL Java_jorgan_creative_SoundFontManager_getDescriptor(JNIEnv *env, jobject obj, jint bank) {
-  // TODO
-  return NULL;
+JNIEXPORT jstring JNICALL Java_jorgan_creative_SoundFontManager_getDescriptor(JNIEnv *env, jclass jclass, jobject jcontext, jint jbank) {
+	throwException(env, "java/lang/UnsupportedOperationException", "not implemented");
+	return NULL;
 }
 	
-JNIEXPORT jstring JNICALL Java_jorgan_creative_SoundFontManager_getPresetDescriptor(JNIEnv *env, jobject obj, jint bank, jint preset) {
-  // TODO
-  return NULL;
+JNIEXPORT jstring JNICALL Java_jorgan_creative_SoundFontManager_getPresetDescriptor(JNIEnv *env, jclass jclass, jobject jcontext, jint jbank, jint jpreset) {
+	throwException(env, "java/lang/UnsupportedOperationException", "not implemented");
+	return NULL;
 }
