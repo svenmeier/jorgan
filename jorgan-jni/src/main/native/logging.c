080b1bda50a7dcc0dc53785684ceae907d53ef71
Sven Meier <sven@meiers.net>
2009-12-13 12:24:11 +0000

    don't cache, reuse functionalty

--- jorgan-jni/src/main/native/logging.c
+++ jorgan-jni/src/main/native/logging.c
@@ -3,48 +3,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void jorgan_warn(JNIEnv* env, char* pattern, ...) {
+static void jorgan_log(JNIEnv* env, char* pattern, char* level, va_list args) {
 
-	static jclass jclass;
+	jclass jclass = (*env)->FindClass(env, "jorgan/jni/Logging");
 	if (jclass == NULL) {
-		jclass = (*env)->FindClass(env, "jorgan/jni/Logging");
-		if (jclass == NULL) {
-			return;
-		}
+		return;
 	}
-	jmethodID mid = (*env)->GetStaticMethodID(env, jclass, "warn", "(Ljava/lang/String;)V");
+	jmethodID mid = (*env)->GetStaticMethodID(env, jclass, level, "(Ljava/lang/String;)V");
 
 	char message[256];
-
-	va_list args;
-	va_start(args, pattern);
 	vsnprintf(message, 256, pattern, args);
-	va_end(args);
 
 	jstring jmessage = (*env)->NewStringUTF(env, message);
 
 	(*env)->CallStaticVoidMethod(env, jclass, mid, jmessage);
+}
+
+void jorgan_warn(JNIEnv* env, char* pattern, ...) {
+
+	va_list args;
+	va_start(args, pattern);
+	jorgan_log(env, pattern, "warn", args);
+	va_end(args);
 }
 
 void jorgan_info(JNIEnv* env, char* pattern, ...) {
 
-	static jclass jclass;
-	if (jclass == NULL) {
-		jclass = (*env)->FindClass(env, "jorgan/jni/Logging");
-		if (jclass == NULL) {
-			return;
-		}
-	}
-	jmethodID mid = (*env)->GetStaticMethodID(env, jclass, "info", "(Ljava/lang/String;)V");
-
-	char message[256];
-
 	va_list args;
 	va_start(args, pattern);
-	vsnprintf(message, 256, pattern, args);
+	jorgan_log(env, pattern, "info", args);
 	va_end(args);
-
-	jstring jmessage = (*env)->NewStringUTF(env, message);
-
-	(*env)->CallStaticVoidMethod(env, jclass, mid, jmessage);
 }
+
+
