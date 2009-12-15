4342be695a65bca17e4e8d9c6c7bd8bd3e6d20a5
svenmeier <svenmeier@c0761391-0d4f-4fe6-b375-9a78db3c6b12>
2009-12-15 19:46:04 +0000

    renamings and constants

--- trunk/jorgan-jni/src/main/native/exception.h
+++ trunk/jorgan-jni/src/main/native/exception.h
@@ -1,3 +1,8 @@
 #include <jni.h>
 
-void jorgan_throwException(JNIEnv* env, char* exception, char* pattern, ...);
+#define ILLEGAL_ARGUMENT_EXCEPTION "java/lang/IllegalArgumentException"
+#define IO_EXCEPTION               "java/io/IOException"
+#define FILE_NOT_FOUND_EXCEPTION   "java/io/FileNotFoundException"
+#define ERROR                      "java/lang/ERROR"
+
+void jorgan_throw(JNIEnv* env, char* exception, char* pattern, ...);
