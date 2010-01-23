d25f548132f155b586f64c0a63cb638bdb6ebf41
Sven Meier <sven@meiers.net>
2010-01-23 20:11:03 +0000

    adjustments to new project structure

--- jorgan-installer/build.bat
+++ jorgan-installer/build.bat
@@ -1,5 +1,5 @@
 set JAVA_HOME="C:\Programme\Java\jdk1.6.0_17"
-set ANT="C:\Programme\apache-ant-1.7.1\bin\ant.bat"
+set ANT="C:\Programme\apache-ant-1.8.0RC1\bin\ant.bat"
 set LAUNCH4J="C:\Programme\Launch4j\launch4jc.exe"
 set INNO_SETUP="C:\Programme\Inno Setup 5\ISCC.exe"
 
