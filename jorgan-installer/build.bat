4c6f83bd81586c61dce54a1ee85696ec653f8ed5
Sven Meier <sven@meiers.net>
2010-01-23 17:44:45 +0000

    corrected paths

--- jorgan-installer/build.bat
+++ jorgan-installer/build.bat
@@ -3,19 +3,19 @@ set ANT="C:\Programme\apache-ant-1.7.1\bin\ant.bat"
 set LAUNCH4J="C:\Programme\Launch4j\launch4jc.exe"
 set INNO_SETUP="C:\Programme\Inno Setup 5\ISCC.exe"
 
-call %ANT% -buildfile ../core/build.xml
-call %ANT% -buildfile ../creative/build.xml
-call %ANT% -buildfile ../customizer/build.xml
-call %ANT% -buildfile ../executor/build.xml
-call %ANT% -buildfile ../fluidsynth/build.xml
-call %ANT% -buildfile ../importer/build.xml
-call %ANT% -buildfile ../keyboard/build.xml
-call %ANT% -buildfile ../linuxsampler/build.xml
-call %ANT% -buildfile ../memory/build.xml
-call %ANT% -buildfile ../midimerger/build.xml
-call %ANT% -buildfile ../recorder/build.xml
-call %ANT% -buildfile ../skins/build.xml
-call %ANT% -buildfile ../soundfont/build.xml
+call %ANT% -buildfile ../jorgan/build.xml
+call %ANT% -buildfile ../jorgan-creative/build.xml
+call %ANT% -buildfile ../jorgan-customizer/build.xml
+call %ANT% -buildfile ../jorgan-executor/build.xml
+call %ANT% -buildfile ../jorgan-fluidsynth/build.xml
+call %ANT% -buildfile ../jorgan-importer/build.xml
+call %ANT% -buildfile ../jorgan-keyboard/build.xml
+call %ANT% -buildfile ../jorgan-linuxsampler/build.xml
+call %ANT% -buildfile ../jorgan-memory/build.xml
+call %ANT% -buildfile ../jorgan-midimerger/build.xml
+call %ANT% -buildfile ../jorgan-recorder/build.xml
+call %ANT% -buildfile ../jorgan-skins/build.xml
+call %ANT% -buildfile ../jorgan-soundfont/build.xml
 
 :Clean
 rmdir /S /Q target
