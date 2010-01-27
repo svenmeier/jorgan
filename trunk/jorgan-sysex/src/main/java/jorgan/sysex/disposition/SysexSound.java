0b04231f6494eb1f89443aac353f5214da3bf9ba
svenmeier <svenmeier@c0761391-0d4f-4fe6-b375-9a78db3c6b12>
2010-01-27 04:32:32 +0000

    added javadoc

--- trunk/jorgan-sysex/src/main/java/jorgan/sysex/disposition/SysexSound.java
+++ trunk/jorgan-sysex/src/main/java/jorgan/sysex/disposition/SysexSound.java
@@ -3,6 +3,13 @@ package jorgan.sysex.disposition;
 import jorgan.disposition.GenericSound;
 import jorgan.util.Null;
 
+/**
+ * A {@link GenericSound} subclass offering sending Sysex messages on opening
+ * and/or closing.
+ * 
+ * @see #getOpen()
+ * @see #getClose()
+ **/
 public class SysexSound extends GenericSound {
 
 	private String open;
@@ -10,11 +17,13 @@ public class SysexSound extends GenericSound {
 	private String close;
 
 	public void setOpen(String open) {
+		// ignore equal values
 		if (!Null.safeEquals(this.open, open)) {
 			String oldOpen = this.open;
 
 			this.open = open;
 
+			// notify listeners of a change
 			fireChange(new PropertyChange(oldOpen, this.open));
 		}
 	}
@@ -24,11 +33,13 @@ public class SysexSound extends GenericSound {
 	}
 
 	public void setClose(String close) {
+		// ignore equal values
 		if (!Null.safeEquals(this.close, close)) {
 			String oldClose = this.close;
 
 			this.close = close;
 
+			// notify listeners of a change
 			fireChange(new PropertyChange(oldClose, this.close));
 		}
 	}
