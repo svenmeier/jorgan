b825349af50169dc8b1e2c3ce261673845adfc59
svenmeier <svenmeier@c0761391-0d4f-4fe6-b375-9a78db3c6b12>
2010-03-17 20:36:40 +0000

    1.6

---
+++ tags/jorgan-fluidsynth-1.6/jorgan-fluidsynth/src/main/java/jorgan/fluidsynth/gui/construct/editor/ChannelsEditor.java
@@ -0,0 +1,9 @@
+package jorgan.fluidsynth.gui.construct.editor;
+
+import jorgan.gui.construct.editor.IntegerEditor;
+
+public class ChannelsEditor extends IntegerEditor {
+	public ChannelsEditor() {
+		super(16, 16, 256);
+	}
+}
---
+++ tags/jorgan-fluidsynth-1.6/jorgan-fluidsynth/src/main/java/jorgan/fluidsynth/gui/construct/editor/PolyphonyEditor.java
@@ -0,0 +1,9 @@
+package jorgan.fluidsynth.gui.construct.editor;
+
+import jorgan.gui.construct.editor.IntegerEditor;
+
+public class PolyphonyEditor extends IntegerEditor {
+	public PolyphonyEditor() {
+		super(16, 1, 4096);
+	}
+}
