70d0d67f1a9f07f6cac3495fef4e17ef67160256
Sven Meier <sven@meiers.net>
2010-05-12 15:30:23 +0000

    new build all

--- jorgan-package/build.sh
+++ jorgan-package/build.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
+
+# adjust:
+# - version in src/debian/changelog
+# - architecture in src/debian/control
 sudo rm *.deb
 
-ant -buildfile ../jorgan/build.xml
-ant -buildfile ../jorgan-importer/build.xml
-ant -buildfile ../jorgan-customizer/build.xml
-ant -buildfile ../jorgan-creative/build.xml
-ant -buildfile ../jorgan-executor/build.xml
-ant -buildfile ../jorgan-fluidsynth/build.xml
-ant -buildfile ../jorgan-keyboard/build.xml
-ant -buildfile ../jorgan-lan/build.xml
-ant -buildfile ../jorgan-linuxsampler/build.xml
-ant -buildfile ../jorgan-memory/build.xml
-ant -buildfile ../jorgan-midimerger/build.xml
-ant -buildfile ../jorgan-recorder/build.xml
-ant -buildfile ../jorgan-sams/build.xml
-ant -buildfile ../jorgan-skins/build.xml
-ant -buildfile ../jorgan-soundfont/build.xml
-ant -buildfile ../jorgan-sysex/build.xml
+ant -buildfile ../build.xml
 
 cd src
 sudo ./rules
