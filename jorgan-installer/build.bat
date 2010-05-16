f7bd955d3f50402bf33dc1511369440db85d02c3
Sven Meier <sven@meiers.net>
2010-05-16 15:49:22 +0000

    re-added call

--- jorgan-installer/build.bat
+++ jorgan-installer/build.bat
@@ -1,3 +1,5 @@
+@echo off
+
 set JAVA_HOME="C:\Programme\Java\jdk1.6.0_17"
 set ANT="C:\Programme\apache-ant-1.8.0RC1\bin\ant.bat"
 set LAUNCH4J="C:\Programme\Launch4j\launch4jc.exe"
@@ -5,7 +7,7 @@ set INNO_SETUP="C:\Programme\Inno Setup 5\ISCC.exe"
 
 rmdir /S /Q target
 
-ant -buildfile ..\build.xml
+call ant -buildfile ..\build.xml
 if errorlevel 1 exit /B 1
 
 mkdir target
