e269e131150526f9428d7e8eae7fde13ea155d18
svenmeier <svenmeier@c0761391-0d4f-4fe6-b375-9a78db3c6b12>
2010-05-30 08:05:56 +0000

    1.1.1 patch for ALL_CTRL_OFF

--- branches/jorgan-fluidsynth-1.7/jorgan-fluidsynth/src/test/java/jorgan/fluidsynth/FluidsynthTest.java
+++ branches/jorgan-fluidsynth-1.7/jorgan-fluidsynth/src/test/java/jorgan/fluidsynth/FluidsynthTest.java
@@ -22,8 +22,11 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-import javax.sound.midi.ShortMessage;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Track;
 
+import jorgan.midi.MessageUtils;
 import junit.framework.TestCase;
 
 /**
@@ -36,7 +39,7 @@ public class FluidsynthTest extends TestCase {
 				"./target/native");
 	}
 
-	public void test() throws Exception {
+	public void testLive() throws Exception {
 		List<String> drivers = Fluidsynth.getAudioDrivers();
 		for (String driver : drivers) {
 			System.out.println(driver);
@@ -51,50 +54,74 @@ public class FluidsynthTest extends TestCase {
 		} catch (IOException expected) {
 		}
 
-		Fluidsynth synth = new Fluidsynth();
+		final Fluidsynth synth = new Fluidsynth();
 
 		synth.soundFontLoad(new File(
 				"./src/main/dispositions/fluidsynth-example.SF2"));
 
-		synth.setTuning(0, 1, "Test", new double[] { 0.0d, 0.0d, -40.0d, 0.0d,
-				0.0d, 0.0d, 0.0d, +40.0d, 0.0d, 0.0d, 0.0d, 0.0d });
+		Recording recording = new Recording() {
+			@Override
+			public synchronized void w() {
+				try {
+					wait(2000);
+				} catch (InterruptedException interrupted) {
+				}
+			}
 
-		synth.send(0, ShortMessage.CONTROL_CHANGE, 121, 0);
-		synth.send(0, ShortMessage.CONTROL_CHANGE, 0, 0);
-		synth.send(0, ShortMessage.PROGRAM_CHANGE, 0, 0);
+			@Override
+			public void on(int cc, int data1, int data2) {
+				synth.send(0, cc, data1, data2);
+			}
+		};
 
-		// select tuningProgram
-		rpn(synth, 0, 3, 1);
-
-		play(synth, 60);
-		play(synth, 62);
-		play(synth, 64);
-		play(synth, 65);
-		play(synth, 67);
-		play(synth, 69);
-		play(synth, 71);
-		play(synth, 72);
-
-		synchronized (this) {
-			wait(1000);
-		}
+		script(recording);
 
 		synth.destroy();
 	}
 
-	private void rpn(Fluidsynth synth, int msb, int lsb, int msbValue) {
-		synth.send(0, ShortMessage.CONTROL_CHANGE, 101, msb);
-		synth.send(0, ShortMessage.CONTROL_CHANGE, 100, lsb);
-		synth.send(0, ShortMessage.CONTROL_CHANGE, 6, msbValue);
+	public void testSequence() throws Exception {
+		final Sequence sequence = new Sequence(Sequence.PPQ, 1);
+
+		Recording recording = new Recording() {
+			private Track track = sequence.createTrack();
+
+			private int tick = 0;
+
+			@Override
+			public void w() {
+				tick += 4;
+			}
+
+			@Override
+			public void on(int cc, int data1, int data2) {
+				track.add(new MidiEvent(MessageUtils.newMessage(cc, data1,
+						data2), tick));
+			}
+		};
+
+		script(recording);
+
+		// MidiSystem.write(sequence, 1, new File("test.mid"));
 	}
 
-	private void play(Fluidsynth synth, int pitch) throws Exception {
-		synth.send(0, ShortMessage.NOTE_ON, pitch, 100);
+	private void script(Recording recording) throws Exception {
+		int CONTROLLER = 11;
 
-		synchronized (this) {
-			wait(1000);
-		}
+		recording.on(176, 121, 0);
+		recording.on(192, 0, 0);
+		recording.on(176, CONTROLLER, 60);
+		recording.w();
+		recording.on(144, 60, 127);
+		recording.w();
+		recording.on(176, CONTROLLER, 60);
+		recording.w();
+		recording.on(128, 60, 0);
+		recording.w();
+	}
 
-		synth.send(0, ShortMessage.NOTE_OFF, pitch, 0);
+	private interface Recording {
+		public void w();
+
+		public void on(int cc, int d1, int d2);
 	}
 }
