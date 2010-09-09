a2f8e8585eb6c14cb2e4af1e8bf26359e557fb14
Sven Meier <sven@meiers.net>
2010-09-09 18:05:54 +0000

    swing libs

--- jorgan/src/main/java/jorgan/swing/ComboBoxUtils.java
+++ jorgan/src/main/java/jorgan/swing/ComboBoxUtils.java
@@ -1,21 +1,31 @@
 package jorgan.swing;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
 
 import javax.swing.ComboBoxModel;
 import javax.swing.DefaultComboBoxModel;
 
 public class ComboBoxUtils {
 
-	public static ComboBoxModel createModel(Object[] items) {
-		return new DefaultComboBoxModel(items);
+	public static <T> ComboBoxModel createModelWithNull(List<T> items) {
+		Vector<Object> vector = new Vector<Object>();
+		vector.add(null);
+		vector.addAll(items);
+		return new DefaultComboBoxModel(vector);
 	}
 
-	public static ComboBoxModel createModelWithNull(Object[] items) {
-		return new DefaultComboBoxModel(withNull(items));
+	public static <T> ComboBoxModel createModelWithNull(T... items) {
+		Vector<Object> vector = new Vector<Object>();
+		vector.add(null);
+		for (Object item : items) {
+			vector.add(item);
+		}
+		return new DefaultComboBoxModel(vector);
 	}
 
-	public static <T> T[] withNull(T[] items) {
+	public static <T> T[] withNull(T... items) {
 		T[] copy = Arrays.copyOf(items, items.length + 1);
 		System.arraycopy(items, 0, copy, 1, items.length);
 		copy[0] = null;
