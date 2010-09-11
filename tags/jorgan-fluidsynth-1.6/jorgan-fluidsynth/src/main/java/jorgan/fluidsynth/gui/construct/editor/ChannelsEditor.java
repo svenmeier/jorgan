8d5e456190f4b77bd281db21c13191950bcded6e
svenmeier <svenmeier@c0761391-0d4f-4fe6-b375-9a78db3c6b12>
2010-09-11 19:08:33 +0000

    obsolete

--- tags/jorgan-fluidsynth-1.6/jorgan-fluidsynth/src/main/java/jorgan/fluidsynth/gui/construct/editor/ChannelsEditor.java
+++
@@ -1,9 +0,0 @@
-package jorgan.fluidsynth.gui.construct.editor;
-
-import jorgan.gui.construct.editor.IntegerEditor;
-
-public class ChannelsEditor extends IntegerEditor {
-	public ChannelsEditor() {
-		super(16, 16, 256);
-	}
-}
