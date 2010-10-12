91b338ac69c5ccd7453c9e8bf0de1dcd7f24b248
svenmeier <svenmeier@c0761391-0d4f-4fe6-b375-9a78db3c6b12>
2010-10-12 09:50:24 +0000

    3.11

---
+++ tags/jorgan-3.11/jorgan-linuxsampler/src/main/java/jorgan/linuxsampler/gui/construct/LinuxsamplerSoundBeanInfo.java
@@ -0,0 +1,40 @@
+/*
+ * jOrgan - Java Virtual Organ
+ * Copyright (C) 2003 Sven Meier
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package jorgan.linuxsampler.gui.construct;
+
+import jorgan.gui.construct.editor.FileEditor;
+import jorgan.gui.construct.editor.IntegerEditor;
+import jorgan.gui.construct.editor.StringEditor;
+import jorgan.gui.construct.info.GenericSoundBeanInfo;
+import jorgan.linuxsampler.disposition.LinuxsamplerSound;
+
+/**
+ * BeanInfo for {@link LinuxsamplerSound}.
+ */
+public class LinuxsamplerSoundBeanInfo extends GenericSoundBeanInfo {
+
+	@Override
+	protected void registerProperties() {
+		super.registerProperties();
+
+		add("host", LinuxsamplerSound.class, StringEditor.class);
+		add("port", LinuxsamplerSound.class, IntegerEditor.class);
+		add("lscp", LinuxsamplerSound.class, FileEditor.class);
+	}
+}
