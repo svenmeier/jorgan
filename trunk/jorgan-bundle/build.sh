f64eb7dee9a7e746730667d6779c5bb3ed648099
svenmeier <svenmeier@c0761391-0d4f-4fe6-b375-9a78db3c6b12>
2010-11-10 18:10:22 +0000

    mac dylib loading fixed

--- trunk/jorgan-bundle/build.sh
+++ trunk/jorgan-bundle/build.sh
@@ -5,3 +5,9 @@ set -e
 ant -buildfile ../build.xml
 
 ant -buildfile build.xml
+
+chmod a+x ./target/jOrgan/jOrgan.app/Contents/MacOS/JavaApplicationStub
+
+hdiutil create -srcfolder ./target/jOrgan ./target/jOrgan.dmg
+
+hdiutil internet-enable -yes ./target/jOrgan.dmg
