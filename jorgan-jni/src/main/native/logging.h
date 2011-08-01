b554b2db496a7d226fc564098d6b531bc096ecf7
Sven Meier <sven@meiers.net>
2011-08-01 17:38:01 +0000

    3.14

--- jorgan-jni/src/main/native/logging.h
+++
@@ -1,8 +0,0 @@
-#include <jni.h>
-
-void jorgan_warn(JNIEnv* env, char* pattern, ...);
-
-void jorgan_info(JNIEnv* env, char* pattern, ...);
-
-void jorgan_severe(JNIEnv* env, char* pattern, ...);
-
--- jorgan/src/main/java/jorgan/disposition/event/AbstractChange.java
+++
@@ -1,8 +0,0 @@
-package jorgan.disposition.event;
-
-public abstract class AbstractChange implements UndoableChange {
-	
-	public boolean replaces(UndoableChange change) {
-		return false;
-	}
-}
