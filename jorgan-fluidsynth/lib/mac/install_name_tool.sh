b0ee78a3ebd21134aae179fa07bde8a1c361ce3f
nikitalita <69168929+nikitalita@users.noreply.github.com>
2021-05-10 15:26:41 -0700

    upgrade macos fluidsynth binaries to 2.2.1

--- jorgan-fluidsynth/lib/mac/install_name_tool.sh
+++ jorgan-fluidsynth/lib/mac/install_name_tool.sh
@@ -5,11 +5,13 @@
 set -e
 
 install_name_tool -id @loader_path/libfluidsynth.dylib libfluidsynth.dylib
-install_name_tool -change /Users/sven/workspace/fluidsynth/build/src/libfluidsynth.1.dylib @loader_path/libfluidsynth.dylib libfluidsynth.dylib
-install_name_tool -change /usr/local/Cellar/glib/2.28.5/lib/libgthread-2.0.0.dylib @loader_path/libgthread-2.0.0.dylib libfluidsynth.dylib
-install_name_tool -change /usr/local/Cellar/glib/2.28.5/lib/libglib-2.0.0.dylib @loader_path/libglib-2.0.0.dylib libfluidsynth.dylib
-install_name_tool -change /usr/local/Cellar/gettext/0.18.1.1/lib/libintl.8.dylib @loader_path/libintl.8.dylib libfluidsynth.dylib
-install_name_tool -change /usr/local/Cellar/libsndfile/1.0.23/lib/libsndfile.1.dylib @loader_path/libsndfile.1.dylib libfluidsynth.dylib
+install_name_tool -change $HOME/workspace/fluidsynth/build/src/libfluidsynth.3.0.1.dylib @loader_path/libfluidsynth.dylib libfluidsynth.dylib
+install_name_tool -change /usr/local/opt/glib/lib/libgthread-2.0.0.dylib @loader_path/libgthread-2.0.0.dylib libfluidsynth.dylib
+install_name_tool -change /usr/local/opt/glib/lib/libglib-2.0.0.dylib @loader_path/libglib-2.0.0.dylib libfluidsynth.dylib
+install_name_tool -change /usr/local/opt/gettext/lib/libintl.8.dylib @loader_path/libintl.8.dylib libfluidsynth.dylib
+install_name_tool -change /usr/local/opt/libsndfile/lib/libsndfile.1.dylib @loader_path/libsndfile.1.dylib libfluidsynth.dylib
+install_name_tool -change /usr/local/opt/portaudio/lib/libportaudio.2.dylib @loader_path/libportaudio.2.dylib libfluidsynth.dylib
+
 otool -L libfluidsynth.dylib | grep "Cellar"
 if test $? -eq 0
 then
@@ -18,7 +20,7 @@ then
 fi
 
 install_name_tool -id @loader_path/libglib-2.0.0.dylib libglib-2.0.0.dylib
-install_name_tool -change /usr/local/Cellar/gettext/0.18.1.1/lib/libintl.8.dylib @loader_path/libintl.8.dylib libglib-2.0.0.dylib
+install_name_tool -change /usr/local/opt/gettext/lib/libintl.8.dylib @loader_path/libintl.8.dylib libglib-2.0.0.dylib
 otool -L libglib-2.0.0.dylib | grep "Cellar"
 if test $? -eq 0
 then
